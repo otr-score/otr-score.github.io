b7bf50e0e9206659f8450b7835b1f96426320fce
Mathis Rosenhauer <rosenhauer@dkrz.de>
2019-08-06 12:19:05 +0200

    deploy

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.e29b8530b3c56efce6e6f7a824737212.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.ba47c6276ea20fb776ae806d7a1964e8.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
   if (!e.data) {
